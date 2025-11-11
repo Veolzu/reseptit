c49797d49cf32832f30842f21e937c3ddce8ad57
Veolzu <83861961+Veolzu@users.noreply.github.com>
2025-11-11 23:16:49 +0200

    added login
    added some very basic login & registering functionality, to be improved

--- app.py
+++ app.py
@@ -1,6 +1,54 @@
+import sqlite3
 from flask import Flask
 from flask import abort, make_response, redirect, render_template, request, session
 import config, users
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+
+        if password1 != password2:
+            return "VIRHE: salasanat eivät ole samat"
+
+        try:
+            users.create_user(username, password1)
+            return redirect("/")
+            
+        except sqlite3.IntegrityError:
+            return "VIRHE: tunnus on jo varattu"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user_id = users.check_login(username, password)
+        if user_id:
+            session["user_id"] = user_id
+            return redirect("/")
+        else:
+            return "VIRHE: väärä tunnus tai salasana"
+
+@app.route("/logout")
+def logout():
+    del session["user_id"]
+    return redirect("/")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
