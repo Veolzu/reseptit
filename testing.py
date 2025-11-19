d96a535cdd2c5933bb1a8d82163d6bb38f31a78e
Veolzu <83861961+Veolzu@users.noreply.github.com>
2025-11-20 00:41:43 +0200

    fix variable names

--- testing.py
+++ testing.py
@@ -9,16 +9,15 @@ db.execute("DELETE FROM ratings")
 db.execute("DELETE FROM recipe_classes")
 
 user_count = 1000
-thread_count = 10**5
-message_count = 10**6
-class_count = 10
+recipe_count = 10**5
+rating_count = 10**6
 classes = ["appetizer", "entree", "dessert", "vegan", "meat", "chinese"] #mexican tag is purposefully missing so you can check if search works
 #so after this data-set mexican tag should get no results
 for i in range(1, user_count + 1):
     db.execute("INSERT INTO users (username) VALUES (?)",
                ["user" + str(i)])
 
-for i in range(1, thread_count + 1):
+for i in range(1, recipe_count + 1):
     user_id = random.randint(1, user_count)
     db.execute("INSERT INTO recipes (title, content, user_id) VALUES (?, ?, ?)",
                ["recipe" + str(i), random.choice(string.ascii_letters), user_id])
@@ -28,9 +27,9 @@ for i in range(1, thread_count + 1):
         sql = "INSERT INTO recipe_classes (recipe_id, title) VALUES (?, ?)"
         db.execute(sql, [i, classes[indice]])
 #rating
-for i in range(1, message_count + 1):
+for i in range(1, rating_count + 1):
     user_id = random.randint(1, user_count)
-    thread_id = random.randint(1, thread_count)
+    thread_id = random.randint(1, recipe_count)
     rating = random.randint(1, 10)
     db.execute("""INSERT INTO ratings (content, rating, user_id, recipe_id)
                   VALUES (?, ?, ?, ?)""",
